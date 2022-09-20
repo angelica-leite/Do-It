5473c7baccd795e10cb660afd5e1089d75dda6a6
Angélica <angeelikasl@gmail.com>
2022-09-20 14:32:44 -0300

    feat: create Input component

--- src/components/Form/input.tsx
+++ src/components/Form/input.tsx
@@ -11,17 +11,43 @@ import {
 import { FaExclamation } from "react-icons/fa";
 
 import { useState, useEffect, useCallback, useRef } from "react";
+import { FieldError } from "react-hook-form";
+import { IconType } from "react-icons/lib";
 
-export const Input = () => {
+interface InputProps extends ChakraInputProps {
+  name: string;
+  label?: string;
+  error?: FieldError | null;
+  icon?: IconType;
+}
+
+export const Input = ({
+  name,
+  error = null,
+  icon: Icon,
+  label,
+  ...rest
+}: InputProps) => {
   return (
     <FormControl>
-      <FormLabel>Label</FormLabel>
+      {!!label && <FormLabel>{label}</FormLabel>}
       <InputGroup flexDirection="column">
-        <InputLeftElement mt="2.5">
-          <FaExclamation />
-        </InputLeftElement>
-        <ChakraInput placeholder="Email" />
-        <FormErrorMessage>Erro</FormErrorMessage>
+        {Icon && (
+          <InputLeftElement mt="2.5">
+            <Icon />
+          </InputLeftElement>
+        )}
+        <ChakraInput
+          name={name}
+          bg="gray.50"
+          variant="outline"
+          _hover={{ bgColor: "gray.100" }}
+          _placeholder={{ color: "gray.300" }}
+          size="lg"
+          h="60px"
+          {...rest}
+        />
+        {!!error && <FormErrorMessage>{error.message}</FormErrorMessage>}
       </InputGroup>
     </FormControl>
   );
